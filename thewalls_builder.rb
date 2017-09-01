6105cf05a7b216501e44ffbb4f8c7256577fa4f5
Skeletorfw <francis.windram@hotmail.co.uk>
2017-09-01 23:09:38 +0100

    Added build number to non-prod runs.

--- thewalls_builder.rb
+++ thewalls_builder.rb
@@ -7,6 +7,7 @@ data = Squib.csv file: 'TheWallsConcept.csv'
 
 buildstring = Time.now.strftime("%d%m%y%H%M")   # Create buildstring
 pdfgen = true
+state = 'Alpha'
 
 Squib::Deck.new cards: data['Title'].size, layout: 'layout.yml' do
   background color: 'white'
@@ -14,14 +15,19 @@ Squib::Deck.new cards: data['Title'].size, layout: 'layout.yml' do
   rect layout: 'safe' # safe zone as defined by TheGameCrafter
   text str: data['Title'], layout: 'title'
   text str: data['Type'], layout: 'type'
-  # icons = data['Gameicon']
-  # svg file: icons.each {|x| GameIcons.get(x).file}, layout: 'art'
   svg file: data['Svgpath'], layout: 'art'
   text str: data['Effect'], layout: 'effect'
   text str: data['Description'], layout: 'description'
   text str: data['Details'], layout: 'details'
+
+  # Only add build details on non-prod runs
+  if state != 'Production'
+    text str: "#{state} build #{buildstring}", layout: 'copyright'
+  end
+
+  # Build cards
   save_png dir: "testdecks/build_#{buildstring}"
   if pdfgen
-    save_pdf dir: "testdecks/build_#{buildstring}", trim: 37.5
+    save_pdf file: "thewalls_build_#{buildstring}", dir: "testdecks/build_#{buildstring}", trim: 37.5
   end
 end
